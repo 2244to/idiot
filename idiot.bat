8d6ea8b905e4ef3512e8e2f8ced1bcbba1bc2b67
2244to <147523946+2244to@users.noreply.github.com>
2025-08-20 16:32:19 +0200

    Update idiot.bat

--- idiot.bat
+++ idiot.bat
@@ -1,4 +1,4 @@
 @echo off
 start "" mspaint "C:\Temp\idiot-main\idiot.gif"
-timeout /t 1 >nul
-powershell -command "$wshell = New-Object -ComObject wscript.shell; $wshell.AppActivate('Paint'); $wshell.SendKeys('^f')"
+timeout /t 2 >nul
+powershell -command "$wshell = New-Object -ComObject wscript.shell; Start-Sleep -Milliseconds 500; $wshell.AppActivate('Paint'); Start-Sleep -Milliseconds 500; $wshell.SendKeys('^f')"
